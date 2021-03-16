dd6288cbb7a23d6a7a1d840dbb129bfada8a4d03
paizsbalazs <GdecodeRPaizs1983>
2021-03-16 17:36:18 +0100

    Megoldás

--- src/main/java/hu/nive/ujratervezes/zarovizsga/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/DigitsCounter.java
@@ -1,10 +1,31 @@
 package hu.nive.ujratervezes.zarovizsga;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DigitsCounter {
 
     public int getCountOfDigits(String s) {
 
-        return 0;
+        if (s != null && !s.isEmpty()) {
+
+            List<Integer> szamok = new ArrayList<>();
+            char[] ch = s.toCharArray();
+
+            for (char c : ch) {
+                if (!szamok.contains(Character.getNumericValue(c))) {
+                    if (c<97 && c != ' ') {
+                        szamok.add(Character.getNumericValue(c));
+                    }
+                }
+            }
+
+            return szamok.size();
+        } else {
+            return 0;
+        }
+
     }
 
 }
